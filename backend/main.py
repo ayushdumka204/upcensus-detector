cd08a0a8503649e1e4e5c5cbdda3328361ea3d02
ayushdumka204 <ayushdumka2005@gmail.com>
2026-09-01 16:56:50 +0530

    dfr

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://upcensus-detector-zph2.vercel.app",
+        "https://upcensus-detector-xsbm.vercel.app/",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
